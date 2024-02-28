93cbfdc17fe59115919d9892509e4a63cc334e57
bachupavani2903 <138902145+bachupavani2903@users.noreply.github.com>
2024-02-28 14:26:37 +0530

    Add files via upload

---
+++ Password-Generator-master/Password Generator/src/GeneratorTest.java
@@ -0,0 +1,40 @@
+class GeneratorTest {
+
+	private final Password password = new Password("Secret");
+	private final Alphabet firstAlphabet = new Alphabet(true, false, false, false);
+	private final Alphabet secondAlphabet = new Alphabet(false, true, true, true);
+	private final Generator generator = new Generator(true, false, false, false);
+
+	@Test
+	void test1() {
+		assertEquals("Secret", password.toString());
+	}
+
+	@Test
+	void test2() {
+		assertEquals(firstAlphabet.getAlphabet(), Alphabet.UPPERCASE_LETTERS);
+	}
+
+	@Test
+	void test3() {
+		assertEquals(secondAlphabet.getAlphabet(), Alphabet.LOWERCASE_LETTERS + Alphabet.NUMBERS + Alphabet.SYMBOLS);
+	}
+
+	@Test
+	void test4() {
+		assertEquals(generator.getAlphabet().getAlphabet(), Alphabet.UPPERCASE_LETTERS);
+	}
+
+	private void assertEquals(String alphabet, String uppercaseLetters) {
+		throw new UnsupportedOperationException("Unimplemented method 'assertEquals'");
+	}
+
+	@Test
+	void test5() {
+		assertEquals(generator.getAlphabet().getAlphabet().length(), 26);
+	}
+
+	private void assertEquals(int length, int i) {
+		throw new UnsupportedOperationException("Unimplemented method 'assertEquals'");
+	}
+}
